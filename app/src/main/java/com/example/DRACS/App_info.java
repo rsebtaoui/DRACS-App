34f8a71591b451502d248fd5f68578aab6676f25
khalil <khalilkml123@gmail.com>
2024-05-25 00:20:47 +0100

    changes: before last clean

---
+++ app/src/main/java/com/example/DRACS/App_info.java
@@ -0,0 +1,40 @@
+package com.example.DRACS;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class App_info extends Fragment {
+
+
+    public App_info() {
+        // Required empty public constructor
+    }
+
+    public static App_info newInstance(String param1, String param2) {
+        App_info fragment = new App_info();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_app_info, container, false);
+
+    }
+}
