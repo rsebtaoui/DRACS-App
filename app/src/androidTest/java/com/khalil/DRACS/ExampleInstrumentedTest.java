86fc999a9016ac139540600fe743a94909db6e40
khalilkml <khalilkml123@gmail.com>
2025-05-05 03:05:59 +0100

    fix: github CI

--- app/src/androidTest/java/com/khalil/DRACS/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/khalil/DRACS/ExampleInstrumentedTest.java
@@ -1,14 +1,13 @@
 package com.khalil.DRACS;
 
-import static org.junit.Assert.assertEquals;
-
 import android.content.Context;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +20,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.DRACS", appContext.getPackageName());
+        assertEquals("com.khalil.DRACS", appContext.getPackageName());
     }
 }
